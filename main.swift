a3a461b8406cd0b30a82b8a413debe1eab713421
20161104603 <1228744270@qq.com>
2018-09-12 11:14:19 +0800

    1

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  l00
+//
+//  Created by 20161104603 on 18/9/5.
+//  Copyright © 2018年 20161104603. All rights reserved.
+//
+
+import Foundation
+
+var a = 10
+var b = 5
+let c = a+b
+print(c)
