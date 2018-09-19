a32967c5862bbe41ca552a51092888b5238cc1b1
20161104603 <1228744270@qq.com>
2018-09-19 11:43:49 +0800

    2

--- main.swift
+++ main.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2018年 20161104603. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 
-var sum = 0
-for i in 1...100{
-    sum = sum + i
-    print(sum)
+//var sum = 0
+//for i in 1...100{
+//    sum = sum + i
+//    print(sum)
+//}
+
+
+//var dd = ["9","6","4","8","7","5","3","2","1"]
+//let d = dd.sorted(by:<)
+//print("\(d)")
+var i = 0
+var arr0: (name:String,age:Int,sex:String) = (name:"黄坚鑫", age:27, sex: "男")
+var arr1 : (name:String,age:Int,sex:String) = (name:"罗锦斌", age:24, sex: "男")
+var arr2 : (name:String,age:Int,sex:String) = (name:"詹佳妮", age:23, sex: "男")
+var arr3 : (name:String,age:Int,sex:String) = (name:"林晓荣", age:29, sex: "男")
+var arr4 : (name:String,age:Int,sex:String) = (name:"陈泽斌", age:19, sex: "男")
+for i in 0...5  {
+    
+    arr[i]<arr[i+1]
 }
+print(arr.0)
+     //   {"name":"罗锦斌", age:"20", sex:"男"},
+      //  {"name":"詹佳妮", age:"20", sex:"女"},
+      //  {"name":"林晓荣", age:"24", sex:"男"},
+      //  {"name":"陈泽斌", age:"22", sex:"男"}
+//];
 
+//arr.sort(function(a,b) {
+ //   return a.age - b.age;
+//})
 
-var dd = ["9","6","4","8","7","5","3","2","1"]
-let d = dd.sorted(by:<)
-print("\(d)")
+//console.log(arr);
